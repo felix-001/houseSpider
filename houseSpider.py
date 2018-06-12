6a7c606ee638863e98af6d7c4292ed8daf5a4681
liyq <279191230@qq.com>
2018-06-12 23:53:05 +0800

    design framework

--- houseSpider.py
+++ houseSpider.py
@@ -9,16 +9,43 @@ from urllib import request,parse
 import re
 
 class Parser(object):
+    def ParsePage():
+        pass
+    def ParseDetail():
+        pass
     pass
 
 class Writer(object):
     pass
 
 class Spider(object):
+    def GetPage(url):
+        pass
+
+    def GetPageNum():
+        pass
+
+    pass
+
+class Logger(object):
+    def log( val ):
+        pass
     pass
 
 
 def main():
+    spider = Spider()
+    logger = Logger()
+    writer = Writer()
+    pageNum = spider.GetPageNum()
+    logger.log( pageNum )
+    for index in range( pageNum ):
+        page = spider.GetPage( pageUrl )
+        urls = parser.ParsePage( page )
+        for url in urls:
+            result = parser.ParseDetail()
+            writer.write( result )
+
 
 if __name__ == '__main__':
     main()
