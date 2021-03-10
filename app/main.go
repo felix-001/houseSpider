318b19b3a345bf45cde8579c2463d83c25ac20f1
liyq <liyuanquan@qiniu.com>
2021-03-10 17:25:02 +0800

    update

--- app/main.go
+++ app/main.go
@@ -8,12 +8,6 @@ import (
 
 const confPath = "/usr/local/etc/house_spider.conf"
 
-func httpCallback(url, body string, err error, opaque interface{}) {
-	log.Println(url)
-	log.Println(err)
-	log.Println(body)
-}
-
 func main() {
 	log.SetFlags(log.Lshortfile)
 	log.Println("enter")
@@ -25,11 +19,4 @@ func main() {
 	proxy := proxy.New(config.ProxyUrl)
 	proxy.Fetch()
 	proxy.Filter()
-
-	//request := request.New(httpCallback)
-	//resp, err := request.Get("https://httpbin.org/get?aaa=bbb", "http://165.227.88.225:8080", nil)
-	//request.AsyncGet("https://httpbin.org/get?aaa=bbb", "http://165.227.88.225:8080", nil)
-	//request.WaitAllDone()
-	//log.Println(err)
-	//log.Println(resp)
 }
