d25b899563f37aea653cb1159dc317556fd5f8a7
rigensen <mr.right.0005@gmail.com>
2021-03-14 17:02:17 +0800

    update

--- conf/conf.go
+++ conf/conf.go
@@ -14,7 +14,9 @@ type Config struct {
 	Groups         []string `json:"groups"`
 	MininumChars   int      `json:"mininum_chars"` // 介绍详情至少要多少个文字
 	MaxPages       int      `json:"max_pages"`     // 每个小组最多爬多少个page
+	MaxDays        int      `json:"max_days"`      // 过了多少天的帖子就不要了
 	BaseUrl        string   `json:"base_url"`
+	ReqInterval    int      `json:"req_interval"`
 }
 
 func Load(confPath string) (*Config, error) {
--- house/house.go
+++ house/house.go
@@ -16,12 +16,11 @@ type House struct {
 	conf                *conf.Config
 	c                   *colly.Collector
 	totalCnt            int
-	title               string
-	content             string
 	contentInvalidCnt   int
 	titleInvalidCnt     int
 	contentTooLittleCnt int            // 介绍详情文字太少了
 	statistics          map[string]int // 统计各个关键字过滤的url个数
+	postTooOldCnt       int
 }
 
 func (h *House) isValid(txt string) (bool, string) {
@@ -35,7 +34,7 @@ func (h *House) isValid(txt string) (bool, string) {
 
 func (s *House) str2time(timestr string) (time.Time, error) {
 	loc, _ := time.LoadLocation("Asia/Shanghai")
-	t, err := time.ParseInLocation("2006-01-02T15:04:05", timestr, loc)
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", timestr, loc)
 	return t, err
 }
 
@@ -45,66 +44,58 @@ func New(conf *conf.Config) *House {
 		colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"),
 	)
 	c.OnHTML("tr[class=pl]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+		defer func() {
+			h.totalCnt++
+		}()
+		link := e.ChildAttr("a[class]", "href")
 		title := e.ChildAttr("td[class=td-subject]>a", "title")
 		if valid, keyword := h.isValid(title); !valid {
 			h.statistics[keyword]++
-			log.Printf("drop %s invalid keyword found in search page, %s, keyword:%s\n", link, title, keyword)
+			log.Printf("drop %s, invalid keyword found in search page, %s, keyword: %s\n", link, title, keyword)
 			return
 		}
+
 		timeStr := e.ChildAttr("td[class=td-time]", "title")
 		tm, _ := h.str2time(timeStr)
 		now := time.Now()
 		duration := now.Sub(tm)
-		if duration.Hours() > 7*24 {
+		if duration.Hours() > float64(h.conf.MaxDays)*24 {
 			log.Printf("drop %s, post too old, time: %s\n", link, timeStr)
+			h.postTooOldCnt++
 			return
 		}
 		c.Visit(e.Request.AbsoluteURL(link))
-		time.Sleep(3 * time.Second)
+		time.Sleep(time.Duration(h.conf.ReqInterval) * time.Second)
 	})
 
-	c.OnHTML("div[class*=rich-content]>p", func(e *colly.HTMLElement) {
-		h.content = e.Text
-	})
-
-	c.OnHTML("td[class=tablecc]", func(e *colly.HTMLElement) {
-		h.title = e.Text
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		url, _ := url.QueryUnescape(r.URL.String())
-		log.Printf("+++++ [%03d] Visiting %s\n", h.totalCnt, url)
-	})
-
-	c.OnScraped(func(resp *colly.Response) {
-		url := resp.Request.URL.String()
-		defer func() {
-			h.title = ""
-			h.content = ""
-		}()
-		if strings.Contains(url, "cat") {
-			return
-		}
-		h.totalCnt++
-		if len(h.content) < h.conf.MininumChars {
-			log.Printf("drop %s, content len < %d actual: %d, content: %s", url, h.conf.MininumChars, len(h.content), h.content)
-			h.contentTooLittleCnt++
-			return
-		}
-		if valid, keyword := h.isValid(h.content); !valid {
-			log.Printf("drop %s, invalid keyword found in content detail, keyword:%s, content: %s", url, keyword, h.content)
-			h.statistics[keyword]++
-			h.contentInvalidCnt++
-			return
-		}
-		if valid, keyword := h.isValid(h.title); !valid {
-			log.Printf("drop %s, invalid keyword found in title, %s, keyword: %s", url, h.title, keyword)
+	c.OnHTML("div[class=topic-doc]", func(e *colly.HTMLElement) {
+		url := e.Request.URL.String()
+		title := e.ChildAttr("td[class=tablecc]", "td")
+		if valid, keyword := h.isValid(title); !valid {
+			log.Printf("drop %s, invalid keyword found in title, %s, keyword: %s", url, title, keyword)
 			h.statistics[keyword]++
 			h.titleInvalidCnt++
 			return
 		}
+		content := e.ChildText("div[class=topic-content]")
+		if len(content) < h.conf.MininumChars {
+			log.Printf("drop %s, content len < %d actual: %d, content: %s", url, h.conf.MininumChars, len(content), content)
+			h.contentTooLittleCnt++
+			return
+		}
+		if valid, keyword := h.isValid(content); !valid {
+			log.Printf("drop %s, invalid keyword found in content detail, keyword:%s, content: %s", url, keyword, content)
+			h.statistics[keyword]++
+			h.contentInvalidCnt++
+			return
+		}
 		h.validUrls = append(h.validUrls, url)
+		log.Printf("append url: %s to valid url list", url)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		url, _ := url.QueryUnescape(r.URL.String())
+		log.Printf("+++++ [%04d] Visiting %s\n", h.totalCnt, url)
 	})
 
 	c.OnError(func(resp *colly.Response, err error) {
@@ -140,14 +131,14 @@ func (h *House) Fetch() {
 					"&start=" + fmt.Sprint(i*50)
 				log.Println(urlNew)
 				h.c.Visit(urlNew)
-				time.Sleep(3 * time.Second)
+				time.Sleep(time.Duration(h.conf.ReqInterval) * time.Second)
 			}
 		}
 	}
-	//h.c.Visit("https://www.douban.com/group/search?cat=1013&group=26926&sort=time&q=%E5%9B%9E%E9%BE%99%E8%A7%82")
 	log.Printf("content chars too little count: %d\n", h.contentTooLittleCnt)
 	log.Printf("content invalid count: %d\n", h.contentInvalidCnt)
 	log.Printf("title invalid count: %d\n", h.titleInvalidCnt)
+	log.Printf("post too old count: %d", h.postTooOldCnt)
 	log.Printf("total: %d", h.totalCnt)
 	for k, v := range h.statistics {
 		log.Printf("%s ==> %d", k, v)
